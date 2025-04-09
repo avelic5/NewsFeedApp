3475dbc5b47c497a129683c4f3dde24b0bb52509
Aldin Velić <avelic5@etf.unsa.ba>
2025-04-09 11:11:21 +0100

    Dodani tagovi

--- app/src/main/java/etf/ri/rma/newsfeedapp/screen/FilterChips.kt
+++ app/src/main/java/etf/ri/rma/newsfeedapp/screen/FilterChips.kt
@@ -10,20 +10,25 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun FilterChips(selectedCategory: String, onCategorySelected: (String) -> Unit) {
     Row(modifier = Modifier.padding(8.dp)) { //unutar reda ih stavljaj
         val categories = listOf("All", "Politika", "Sport", "Nauka/tehnologija")
-        categories.forEach { category ->
+        val lista = listOf("filter_chip_all", "filter_chip_pol", "filter_chip_spo", "filter_chip_sci")
+
+        categories.forEachIndexed { index, category ->
             AssistChip( // composable komponenta koja predstavlja čip koji se može kliknuti.
                 onClick = { onCategorySelected(category) }, // Lambda funkcija koja se poziva kada se čip klikne
                 //onCategorySelect je proslijedjen kao parametar funkciji
                 colors = AssistChipDefaults.assistChipColors(  //za boje
                     containerColor = if (category == selectedCategory) Color.Gray else Color.LightGray
                 ),
-                modifier = Modifier.padding(4.dp),
+                modifier = Modifier
+                    .padding(4.dp)
+                    .testTag(lista[index]),
                 label = { Text(text = category) }
             )
         }
