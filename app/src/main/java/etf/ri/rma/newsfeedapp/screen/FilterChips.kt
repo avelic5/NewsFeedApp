6977c31050daeeef356b8001fb0439e9f96d25af
Aldin Velić <avelic5@etf.unsa.ba>
2025-04-12 01:02:57 +0100

    Dodan novi chip

--- app/src/main/java/etf/ri/rma/newsfeedapp/screen/FilterChips.kt
+++ app/src/main/java/etf/ri/rma/newsfeedapp/screen/FilterChips.kt
@@ -1,29 +1,26 @@
-package etf.ri.rma.newsfeedapp.screen
-
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AssistChip
 import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun FilterChips(selectedCategory: String, onCategorySelected: (String) -> Unit) {
-    Row(modifier = Modifier.padding(8.dp)) { //unutar reda ih stavljaj
-        val categories = listOf("All", "Politika", "Sport", "Nauka/tehnologija")
-        val lista = listOf("filter_chip_all", "filter_chip_pol", "filter_chip_spo", "filter_chip_sci")
+    FlowRow(modifier = Modifier.padding(8.dp)) {
+        val categories = listOf("All", "Politika", "Sport", "Nauka/tehnologija", "Muzika")
+        val lista = listOf("filter_chip_all", "filter_chip_pol", "filter_chip_spo", "filter_chip_sci", "filter_chip_none")
 
         categories.forEachIndexed { index, category ->
-            AssistChip( // composable komponenta koja predstavlja čip koji se može kliknuti.
-                onClick = { onCategorySelected(category) }, // Lambda funkcija koja se poziva kada se čip klikne
-                //onCategorySelect je proslijedjen kao parametar funkciji
-                colors = AssistChipDefaults.assistChipColors(  //za boje
+            AssistChip(
+                onClick = { onCategorySelected(category) },
+                colors = AssistChipDefaults.assistChipColors(
                     containerColor = if (category == selectedCategory) Color.Gray else Color.LightGray
                 ),
                 modifier = Modifier
--- app/src/main/java/etf/ri/rma/newsfeedapp/screen/NewsFeedScreen.kt
+++ app/src/main/java/etf/ri/rma/newsfeedapp/screen/NewsFeedScreen.kt
@@ -1,5 +1,6 @@
 package etf.ri.rma.newsfeedapp.screen
 
+import FilterChips
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
